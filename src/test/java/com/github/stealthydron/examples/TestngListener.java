c9df40717423bb7e32cc02e963102da974c4a023
qwerty <dron_082.1@mail.ru>
2022-08-26 13:48:23 +0300

    test

--- src/test/java/com/github/stealthydron/examples/TestngListener.java
+++ src/test/java/com/github/stealthydron/examples/TestngListener.java
@@ -11,11 +11,11 @@ public class TestngListener extends TestListenerAdapter implements IInvokedMetho
     @Override
     public void afterInvocation(IInvokedMethod iInvokedMethod, ITestResult result) {
         if (iInvokedMethod.isConfigurationMethod() && result.getStatus() == ITestResult.FAILURE) {
-            Allure.addAttachment("on test failure attachment", "on test failure attachment content");
+            Allure.addAttachment("on configuration failure attachment", "on configuration failure attachment content");
         }
 
         if (!iInvokedMethod.isConfigurationMethod() && result.getStatus() == ITestResult.FAILURE) {
-            Allure.addAttachment("on configuration failure attachment", "on configuration failure attachment content");
+            Allure.addAttachment("on test failure attachment", "on test failure attachment content");
         }
     }
 }
---
+++ src/test/java/com/github/stealthydron/examples/test/CreateWorkItemDemo2Test.java
@@ -0,0 +1,42 @@
+package com.github.stealthydron.examples.test;
+
+import com.github.avpyanov.testit.annotations.AutotestId;
+import com.github.stealthydron.example.steps.MainSteps;
+import com.github.stealthydron.examples.TestngListener;
+import com.github.stealthydron.examples.samples.TestSteps;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+import static com.github.stealthydron.examples.app.StaticExample.doSomething;
+import static com.github.stealthydron.examples.app.StaticExample.getSomething;
+import static org.testng.Assert.assertEquals;
+
+@Listeners({TestngListener.class})
+public class CreateWorkItemDemo2Test {
+
+    private final TestSteps steps = new TestSteps();
+    private final MainSteps mainSteps = new MainSteps();
+
+    @BeforeMethod
+    public void setup() {
+        assertEquals(2, 1);
+    }
+
+    @AutotestId("35")
+    @Epic("Create Work Item Demo Epic")
+    @Feature("Create Work Item Demo Epic Feature")
+    @Story("Create Work Item Demo Story")
+    @Test(description = "Test should fail on setup")
+    public void testMethodExample5() {
+        String text = getSomething();
+        doSomething(text);
+        steps.testStep1().testStep2();
+        mainSteps.mainStep1();
+        mainSteps.mainStep2();
+        mainSteps.assertThatSomethingFail();
+    }
+}
